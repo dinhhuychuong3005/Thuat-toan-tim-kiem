cbafbd5512f69583b5f4581ada8074b3a5aa88c6
dinhhuychuong3005 <dinhhuychuong3005@gmail.com>
2021-05-24 16:00:43 +0700

    done

--- src/bt4/Main.java
+++ src/bt4/Main.java
@@ -1,17 +1,40 @@
 package bt4;
 
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        int[] list = {1,3,5,7,9,11,13,15,17,19,99};
-        System.out.println("Nhập số cần tìm:");
         Scanner scanner = new Scanner(System.in);
+        System.out.println("Nhập vào số phần tử của mảng:");
+        int size = scanner.nextInt();
+        int[] arr = new int[size];
+        System.out.println("Nhập vào các phần tử của mảng");
+        for (int i = 0; i < size; i++) {
+            System.out.println("nhập vào phần tử " + (i + 1));
+            arr[i] = scanner.nextInt();
+        }
+        int temp;
+        for (int i = 0; i < arr.length; i++) {
+            for (int j =arr.length - 1;j>i;j--){
+                if (arr[i] > arr[j]){
+                    temp = arr[j];
+                    arr[j] = arr[i];
+                    arr[i] = temp;
+                }
+            }
+
+        }
+        for (Integer integer : arr){
+            System.out.println(integer);
+        }
+        System.out.println("Nhập số cần tìm:");
+
         int searchNumber = scanner.nextInt();
-        if (BinarySearch.binarySearch(list,0,list.length-1,searchNumber) == -1){
+        if (BinarySearch.binarySearch(arr, 0, arr.length - 1, searchNumber) == -1) {
             System.out.println("không có số cần tìm trong mảng");
-        }else {
-            System.out.println("số cần tìm: " + searchNumber + " có trong mảng");
+        } else {
+            System.out.println("số cần tìm: " + searchNumber + " có trong mảng, ở vị trí :" + BinarySearch.binarySearch(arr,0,arr.length - 1,searchNumber));
         }
     }
 }
---
+++ src/bt5/Main.java
@@ -0,0 +1,27 @@
+package bt5;
+
+import java.util.LinkedList;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter a string:");
+        String string = scanner.nextLine();
+        LinkedList<Character> max = new LinkedList<>();
+        LinkedList<Character> list = new LinkedList<>();
+        for (int i = 0; i < string.length(); i++) {
+            if (list.size() > 1 && string.charAt(i) <= list.getLast() && list.contains(string.charAt(i))) {
+                list.clear();
+            }
+            list.add(string.charAt(i));
+            if (list.size() > max.size()){
+                max.clear();
+                max.addAll(list);
+            }
+        }
+        for (Character character : max){
+            System.out.println(character);
+        }
+    }
+}
